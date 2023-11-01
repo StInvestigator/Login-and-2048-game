bbf0b74c9d451cd7bb56628200c5e92c978574e5
Murashko Vladislav <vladislavuh2000@gmail.com>
2023-11-01 20:52:04 +0200

    Its done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,57 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
 namespace Login_and_2048_game
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+        public static List<string> errors;
+        public string Name { get; set; }
+        public string Password { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            errors = new List<string>();
+            errors.Add("Field is required");
+            PrintErrors();
+            DataContext = this;
+        }
+
+        private void PropertyChanged(object sender, TextChangedEventArgs e)
+        {
+            Name = TBLogin.Text;
+        }
+
+        private void PropertyChanged2(object sender, TextChangedEventArgs e)
+        {
+            Password = TBPassword.Text;
+            PrintErrors();
+        }
+
+        public void PrintErrors()
+        {
+            SPPV1.Children.Clear();
+            SPPV2.Children.Clear();
+            SPPV3.Children.Clear();
+            Label lab;
+            int count = 0;
+            foreach (var item in errors)
+            {
+                count++;
+                lab = new Label();
+                lab.Foreground = new SolidColorBrush(Colors.Red);
+                lab.Content = item;
+                lab.FontSize = 15;
+                if (count <= 6 && count > 4) SPPV3.Children.Add(lab);
+                if (count <= 4 && count > 2) SPPV2.Children.Add(lab);
+                if (count<=2) SPPV1.Children.Add(lab);
+            }
         }
     }
 }
---
+++ NotEmptyValidationRule.cs
@@ -0,0 +1,68 @@
+﻿using Login_and_2048_game;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MaterialDesignDemo.Domain
+{
+
+
+public class NotEmptyValidationRule : ValidationRule
+{
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        string password = (value ?? "").ToString();
+        if (password?.Length == 0) return new ValidationResult(false, "Field is required");
+        if (password?.Length <= 8) return new ValidationResult(false, "At least 8 digits");
+        return ValidationResult.ValidResult;
+    }
+}
+
+public class PasswordValidateRule : ValidationRule
+{
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        MainWindow.errors.Clear();
+        string password = (value ?? "").ToString();
+        bool isValidationFalse = false;
+        if (password?.Length == 0) { isValidationFalse = true; MainWindow.errors.Add("Field is required"); }
+        if (password?.Length <= 8) { isValidationFalse = true; MainWindow.errors.Add("At least 8 digits"); }
+        bool ContBigLetter = false;
+        bool ContSmallLetter = false;
+        bool ContNumber = false;
+        bool ContSpace = false;
+            foreach (var item in password ?? "")
+        {
+            if (ContBigLetter && ContSmallLetter && ContNumber && ContSpace) break;
+            
+            if (item == 32) ContSpace = true;
+            if (item >= 48 && item <= 57)
+            {
+                ContNumber = true;
+            }
+            if (item >= 65 && item <= 90)
+            {
+                ContBigLetter = true;
+            }
+            else if (item >= 97 && item <= 122)
+            {
+                ContSmallLetter = true;
+            }
+        }
+        if (!ContBigLetter) { isValidationFalse = true; MainWindow.errors.Add("Need to contain a big letter"); }
+            if (!ContSmallLetter) { isValidationFalse = true; MainWindow.errors.Add("Need to contain a moll letter"); }
+            if (!ContNumber) { isValidationFalse = true; MainWindow.errors.Add("Need to contain a number"); }
+            if (ContSpace) { isValidationFalse = true; MainWindow.errors.Add("White spaces is not allower"); }
+
+            if (isValidationFalse)
+            {
+                return new ValidationResult(false,"");
+            }
+            return ValidationResult.ValidResult;
+    }
+}
+}
